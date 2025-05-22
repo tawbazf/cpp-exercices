1d3135f87acfca13ea58e6d65cbd9a547917f216
tawbazf <tawbazehaf@gmail.com>
2025-05-22 12:34:42 +0100

    brouillion

---
+++ brouillion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main (){
+   cout << "age";
+   int age;
+   cin>>age;
+   int age_in_days= age*365;
+   int age_in_hours= age_in_days*24;
+   cout << "age in days is"<< age_in_days<<"days";
+ cout << "age in hours is"<< age_in_hours<<"hours";
+}
+// \n
+// \\ 
+// \"
+// \t
+// \b
+// \a
+// \r
---
+++ ex16.cpp
@@ -0,0 +1,14 @@
+// Écrire une classe C++ appelée « Etudiant » avec les membres suivant :
+// nom : (de type char),
+// note1, note2 : (de type float)
+// calc_moy() : calcule la note moyenne.
+// afficher () : affiche le nom et la note moyenne.
+// Le programme principal (main) demande à l’utilisateur d’entrer le nom et les notes d'un étudiant. et affiche leur nom et la note moyenne.
+
+#include<iostream>
+using namespace std;
+class Etudiant{
+    public: char nom,
+    float note1,note2;
+    float calc_moy()
+}
